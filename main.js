c81f2db8f6938fcc9882d4c090d6a834b0957048
AuroreGrangier <38687379+AuroreGrangier@users.noreply.github.com>
2018-06-14 15:20:31 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,56 @@
+$(document).ready(function() {
+    //open modal
+    $('open_modal').click(function() {
+        $('modal_to_open').css(
+            {
+                'display' : 'block'
+            }
+        );
+    });
+
+    // close modal
+    $('#close_modal').click(function() {
+        $('modal_to_open').css(
+            {
+                'display' : 'none'
+            }
+        )
+    });
+
+    // send mail with ajax
+    $('#send_email').click(function(e){
+		e.preventDefault();
+		var data = {
+			email: $('#email').val(),
+			name: $('#name').val(),
+			objet: $('#firstname').val(),
+			message: $('#message').val()
+        };
+        // AJAX
+		$.ajax({
+			url: "mail.php",
+			type: 'POST',
+			data: data,
+			success: function(data) {
+				$('#js_alert_success').css({'display' : 'block'});
+				setTimeout(function(){
+					$('#js_alert_success').css({'display' : 'none'});
+					$('#email').val("");
+					$('#name').val("");
+					$('#object').val("");
+					$('#message').val("")
+				}, 3000);
+			},
+			error: function(data) {
+				$('#js_alert_danger').css({'display' : 'block'});
+				setTimeout(function(){
+					$('#js_alert_danger').css({'display' : 'none'});
+					$('#email').val("");
+					$('#name').val("");
+					$('#object').val("");
+					$('#message').val("")
+				}, 3000);
+			}
+		});
+	});
+});
